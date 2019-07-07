f2b77aac918051b7e5d77805469ca8bf0a0b944e
bengbengxiong <1620585746@qq.com>
2019-07-07 12:25:11 +0800

    day17

---
+++ day17/day17/源.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*int a[] = { 1, 2, 3, 4, 5 };
+	int *p[] = { a, a + 1, a + 2, a + 3 };
+	int **q = p;
+	cout << *(p[0] + 1) + **(q + 2) << endl;
+	cout << *(p[0] + 1) << endl;
+	cout << **(q + 2) << endl;*/
+	
+	
+	/*char *p = "abc";
+	char *q = "abc123";
+	while (*p == *q)
+	{
+		printf("%c %c", *p, *q);
+	}*/
+
+	long long a = 1, b = 2, c = 3;
+	printf("%d %d %d", a, b, c);
+	system("pause");
+	return 0;
+}
